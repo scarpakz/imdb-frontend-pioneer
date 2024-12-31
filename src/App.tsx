e291d36f84e9b3f6d84e3d729263e5a72b2a24c9
Jason Javier <jasonjjavier@gmail.com>
2024-12-31 18:36:10 -0400

    added Routes to the function App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import {Home} from './pages/Home'
 
@@ -5,7 +6,9 @@ function App() {
 
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   )
 }
